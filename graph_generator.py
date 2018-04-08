c465331729e38f3cee0a1ab2617c55169cb4c053
Aliossandro <alessandro.piscopo@soton.ac.uk>
2018-04-08 18:25:25 +0100

    userframes fixed less than 10

--- graph_generator.py
+++ graph_generator.py
@@ -10,7 +10,7 @@ import matplotlib.ticker as ticker
 # matplotlib.use('WX')
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
-
+import matplotlib.dates as mdates
 # import matplotlib.mlab as mlab
 
 
@@ -44,6 +44,8 @@ wdStats_3['timeframe'] = pd.to_datetime(wdStats_3['index'])
 wdStats_3.sort_values(by='timeframe', inplace=True)
 wdStats_3['month'] = wdStats_3['timeframe'].apply(lambda x: x.strftime('%B %Y'))
 
+wdStats['noInstances'] = wdStats['avgPop'] * wdStats['noClasses']
+wdStats['trueRichness'] = wdStats['classesWInstances']/wdStats['noClasses']
 
 # ###create grid
 # g = sns.FacetGrid(wdStats, col=['avgDepth', 'iRichness', 'cRichness'], hue=['avgDepth', 'iRichness', 'cRichness'], col_wrap=3, )
@@ -101,8 +103,52 @@ def myticks_root(x, pos):
     # return r"${:2.0f}\times 10^{{{:2d}}}$".format(coeff,exponent)
     return coeff
 
+###no classes
+f2,((ax1,ax2, ax3)) = plt.subplots(1,3, sharex='col')
 
-f,((ax1,ax2, ax3), (ax4,ax5, ax6),(ax7, ax8, ax9)) = plt.subplots(3,3, sharex='col')
+font = {'size': 10}
+
+matplotlib.rc('font', **font)
+
+ax1.plot(wdStats['timeframe'],wdStats['noClasses'],  marker='.', markevery=0.05)
+ax1.plot(wdStats['timeframe'],wdStats['noRoot'], marker='x', markevery=0.05)
+ax1.plot(wdStats['timeframe'],wdStats['noLeaf'], marker='^', markevery=0.05)
+ax1.grid(color='gray', linestyle='--', linewidth=.5)
+# forceAspect(ax1,aspect=1)
+ax1.set_ylabel('No. Classes')
+
+ax2.plot(wdStats['timeframe'],wdStats['noInstances'])
+ax2.grid(color='gray', linestyle='--', linewidth=.5)
+
+ax2.set_ylabel('No. Instances')
+
+ax3.plot(wdStats['timeframe'],wdStats['noProps'])
+ax3.grid(color='gray', linestyle='--', linewidth=.5)
+
+ax3.set_ylabel('No. Properties')
+
+ax1.xaxis.set_major_locator(mdates.MonthLocator(interval=8))   #to get a tick every 15 minutes
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
+ax2.xaxis.set_major_locator(mdates.MonthLocator(interval=8))   #to get a tick every 15 minutes
+ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
+ax3.xaxis.set_major_locator(mdates.MonthLocator(interval=8))   #to get a tick every 15 minutes
+ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
+
+f2.autofmt_xdate()
+
+# ax.set_aspect('equal')
+# plt.axes().set_aspect('equal')
+
+
+# f.legend([ax4], 'cosi')
+# plt.ylim(-width)
+# plt.yticks(range(length), domains[0][0:length])
+
+# plt.tight_layout()
+plt.show()
+
+
+f,((ax1,ax2, ax3), (ax4,ax5, ax6)) = plt.subplots(2,3, sharex='col')
 
 font = {'size': 10}
 
@@ -115,7 +161,7 @@ ax1.plot(wdStats['timeframe'],wdStats['avgPop'], marker='.', markevery=0.05)
 ax1.grid(color='gray', linestyle='--', linewidth=.5)
 ax1.set_ylabel('Avg. population')
 # ax1.yaxis.set_label_position('top')
-ax2.plot(wdStats['timeframe'], wdStats['cRichness'])
+ax2.plot(wdStats['timeframe'], wdStats['trueRichness'])
 ax2.grid(color='gray', linestyle='--', linewidth=.5)
 ax2.set_ylabel('Class richness')
 ax3.plot(wdStats['timeframe'],wdStats['iRichness'])
@@ -139,21 +185,21 @@ ax6.plot(wdStats['timeframe'],wdStats['noProps'])
 ax6.grid(color='gray', linestyle='--', linewidth=.5)
 ax6.set_ylabel(r'Properties ($n*10^2$)')
 ax6.yaxis.set_major_formatter(ticker.FuncFormatter(myticks_prop))
-
-ax7.plot(wdStats['timeframe'],wdStats['noClasses'])
-ax7.grid(color='gray', linestyle='--', linewidth=.5)
-ax7.set_ylabel(r'Classes ($n*10^4$)')
-ax7.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-
-ax8.plot(wdStats['timeframe'],wdStats['noLeaf'])
-ax8.grid(color='gray', linestyle='--', linewidth=.5)
-ax8.set_ylabel(r'Leaf classes ($n*10^4$)')
-ax8.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
-
-ax9.plot(wdStats['timeframe'],wdStats['noRoot'])
-ax9.grid(color='gray', linestyle='--', linewidth=.5)
-ax9.set_ylabel(r'Root classes ($n*10^3$)')
-ax9.yaxis.set_major_formatter(ticker.FuncFormatter(myticks_root))
+#
+# ax7.plot(wdStats['timeframe'],wdStats['noClasses'])
+# ax7.grid(color='gray', linestyle='--', linewidth=.5)
+# ax7.set_ylabel(r'Classes ($n*10^4$)')
+# ax7.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
+#
+# ax8.plot(wdStats['timeframe'],wdStats['noLeaf'])
+# ax8.grid(color='gray', linestyle='--', linewidth=.5)
+# ax8.set_ylabel(r'Leaf classes ($n*10^4$)')
+# ax8.yaxis.set_major_formatter(ticker.FuncFormatter(myticks))
+#
+# ax9.plot(wdStats['timeframe'],wdStats['noRoot'])
+# ax9.grid(color='gray', linestyle='--', linewidth=.5)
+# ax9.set_ylabel(r'Root classes ($n*10^3$)')
+# ax9.yaxis.set_major_formatter(ticker.FuncFormatter(myticks_root))
 
 
 
@@ -161,7 +207,7 @@ ax9.yaxis.set_major_formatter(ticker.FuncFormatter(myticks_root))
 # plt.xlabel(r'$\%$ on the Total Number of Matches')
 
 
-import matplotlib.dates as mdates
+
 # # locator = mdates.AutoDateLocator()
 # ax1.fmt_xdata = mdates.DateFormatter('%B %Y')
 # ax2.fmt_xdata = mdates.DateFormatter('%B %Y')
@@ -173,12 +219,12 @@ import matplotlib.dates as mdates
 # ax8.fmt_xdata = mdates.DateFormatter('%B %Y')
 # ax9.fmt_xdata = mdates.DateFormatter('%B %Y')
 
-ax7.xaxis.set_major_locator(mdates.MonthLocator(interval=8))   #to get a tick every 15 minutes
-ax7.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
-ax8.xaxis.set_major_locator(mdates.MonthLocator(interval=8))   #to get a tick every 15 minutes
-ax8.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
-ax9.xaxis.set_major_locator(mdates.MonthLocator(interval=8))   #to get a tick every 15 minutes
-ax9.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
+ax4.xaxis.set_major_locator(mdates.MonthLocator(interval=6))   #to get a tick every 15 minutes
+ax4.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
+ax5.xaxis.set_major_locator(mdates.MonthLocator(interval=6))   #to get a tick every 15 minutes
+ax5.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
+ax6.xaxis.set_major_locator(mdates.MonthLocator(interval=6))   #to get a tick every 15 minutes
+ax6.xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))     #optional formatting
 
 f.autofmt_xdate()
 
@@ -192,3 +238,5 @@ plt.savefig("ontometrics.pdf", format="pdf")
 plt.savefig('ontometrics.eps', format='eps', transparent=True)
 
 plt.show()
+
+
