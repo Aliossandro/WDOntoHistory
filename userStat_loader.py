6a8761376d3699478d2b37da5031cc8449907cdd
Aliossandro <alessandro.piscopo@soton.ac.uk>
2018-04-12 16:08:09 +0100

    modified edits

--- userStat_loader.py
+++ userStat_loader.py
@@ -67,8 +67,16 @@ def fileLoader(path):
     frame = pd.concat(list_)
     frame.columns = ['username', 'noEdits', 'noItems', 'noOntoEdits', 'noPropEdits', 'noCommEdits', 'noTaxoEdits',
                      'noBatchEdits', 'minTime', 'timeframe', 'userAge']
-
     frame = frame.drop(['minTime'], axis=1)
+
+    #read and prepare count of modifying edits
+    mod_file = path + '/modified_count.csv'
+    frame_mod = pd.read_csv(mod_file)
+    frame_mod['timeframe'] = frame_mod['monthinfo'].apply(lambda x: x.replace(' 00:00:00', ''))
+    # frame_mod.timeframe = pd.to_datetime(frame_mod['timeframe'])
+    frame_mod.drop('monthinfo', axis=1, inplace=True)
+
+    frame = frame.merge(frame_mod, how='left', on=['username', 'timeframe'])
     frame['editNorm'] = frame['noEdits']
     frame_anon = frame.loc[frame['username'].str.match(
         r'([0-9]{1,3}[.]){3}[0-9]{1,3}|(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])[.]){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])[.]){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))',
@@ -93,6 +101,7 @@ def fileLoader(path):
     frame_norm['noTaxoEdits'] = frame['noTaxoEdits'] / frame['noEdits']
     frame_norm['noOntoEdits'] = frame['noOntoEdits'] / frame['noEdits']
     frame_norm['noPropEdits'] = frame['noPropEdits'] / frame['noEdits']
+    frame_norm['noeditsmonthly'] = frame['noeditsmonthly'] / frame['noEdits']
     frame_norm['noEdits'] = frame['noEdits']
     # frame_norm = frame_norm.loc[frame_norm['noEdits'] >= 5,]
     frame_norm.reset_index(inplace=True)
@@ -185,9 +194,6 @@ def fileLoader(path):
         f.write(str(things))
         f.close()
     print('gap done')
-
-
-
 
 
 
